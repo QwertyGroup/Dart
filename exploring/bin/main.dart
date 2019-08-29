e4597bf6a4d42dedc55c5257e1dfa25da9fc7f35
Arseniy Kucher <beeffallo@gmail.com>
2019-08-29 14:28:51 +0300

    exploring dart project

---
+++ exploring/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:exploring/exploring.dart' as exploring;
+
+main(List<String> arguments) {
+  print('Hello world: ${exploring.calculate()}!');
+}
---
+++ exploring/test/exploring_test.dart
@@ -0,0 +1,8 @@
+import 'package:exploring/exploring.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
