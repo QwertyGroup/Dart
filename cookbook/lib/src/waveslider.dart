042677ee9de3ee9a6b8f7d1e3acfe5046bc0805e
Arseniy Kucher <beeffallo@gmail.com>
2019-09-15 19:36:11 +0300

    wave slider section: 2

--- cookbook/lib/src/waveslider.dart
+++ cookbook/lib/src/waveslider.dart
@@ -14,10 +14,12 @@ class WaveApp extends StatelessWidget {
 class WaveSlider extends StatefulWidget {
   final double width;
   final double height;
+  final Color color;
 
   const WaveSlider({
     this.width = 350,
     this.height = 50,
+    this.color = Colors.black,
   });
 
   @override
@@ -66,12 +68,12 @@ class _WaveSliderState extends State<WaveSlider> {
         child: Container(
           width: widget.width,
           height: widget.height,
-          color: Colors.red,
-          child: Column(
-            children: <Widget>[
-              Text('drag pos: $_dragPosition'),
-              Text('drag %  : $_dragPercentage'),
-            ],
+          child: CustomPaint(
+            painter: WavePainter(
+              color: widget.color,
+              sliderPosition: _dragPosition,
+              dragPercentage: _dragPercentage,
+            ),
           ),
         ),
         onHorizontalDragUpdate: (update) => _onDragUpdate(context, update),
@@ -80,4 +82,51 @@ class _WaveSliderState extends State<WaveSlider> {
       ),
     );
   }
+}
+
+class WavePainter extends CustomPainter {
+  final double sliderPosition;
+  final double dragPercentage;
+  final Color color;
+  final Paint fillPainter;
+  final Paint wavePainter;
+
+  WavePainter({
+    @required this.sliderPosition,
+    @required this.dragPercentage,
+    @required this.color,
+  })  : fillPainter = Paint()
+          ..color = color
+          ..style = PaintingStyle.fill,
+        wavePainter = Paint()
+          ..color = color
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 2.5;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _paintAnchors(canvas, size);
+    _paintLine(canvas, size);
+    _paintBlock(canvas, size);
+  }
+
+  _paintAnchors(Canvas canvas, Size size) {
+    canvas.drawCircle(Offset(0, size.height), 5, fillPainter);
+    canvas.drawCircle(Offset(size.width, size.height), 5, fillPainter);
+  }
+
+  _paintLine(Canvas canvas, Size size) {
+    Path path = Path();
+    path.moveTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    canvas.drawPath(path, wavePainter);
+  }
+
+  _paintBlock(Canvas canvas, Size size) {
+    Rect rect = Offset(sliderPosition, size.height - 5) & Size(3, 10);
+    canvas.drawRect(rect, fillPainter);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
