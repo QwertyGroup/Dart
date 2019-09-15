fbe03feaf2ef6540b16e6f06cecadc1743487349
Arseniy Kucher <beeffallo@gmail.com>
2019-09-15 20:28:10 +0300

    wave slider section: 4

--- cookbook/lib/src/waveslider.dart
+++ cookbook/lib/src/waveslider.dart
@@ -1,3 +1,8 @@
+// Original video playlist
+// https://www.youtube.com/watch?v=Ze00Ws7c-Qc&list=PLjr4ufdmNA4J2-KwMutexAjjf_VmjL1eH
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WaveApp extends StatelessWidget {
@@ -87,7 +92,10 @@ class _WaveSliderState extends State<WaveSlider> {
 class WavePainter extends CustomPainter {
   final double sliderPosition;
   final double dragPercentage;
+
   final Color color;
+  double _previousSliderPosition = 0;
+
   final Paint fillPainter;
   final Paint wavePainter;
 
@@ -116,13 +124,13 @@ class WavePainter extends CustomPainter {
     canvas.drawCircle(Offset(size.width, size.height), 5, fillPainter);
   }
 
-  _paintWaveLine(Canvas canvas, Size size) {
+  WaveCurveDefinitions _calculateWaveCurveDefinitions() {
     double bendWidth = 40;
     double bezierWidth = 40;
 
     double startOfBend = sliderPosition - bendWidth / 2;
-    double startOfBezier = startOfBend - bezierWidth;
     double endOfBend = sliderPosition + bendWidth / 2;
+    double startOfBezier = startOfBend - bezierWidth;
     double endOfBezier = endOfBend + bezierWidth;
 
     double controlHeight = 0;
@@ -133,32 +141,70 @@ class WavePainter extends CustomPainter {
     double rightControlPoint1 = endOfBend;
     double rightControlPoint2 = endOfBend;
 
+    double bendability = 25;
+    double maxSlideDifference = 20;
+
+    double slideDifference = (sliderPosition - _previousSliderPosition).abs();
+    if (slideDifference > maxSlideDifference) {
+      slideDifference = maxSlideDifference;
+    }
+
+    double bend = lerpDouble(
+      0,
+      bendability,
+      slideDifference / maxSlideDifference,
+    );
+
+    bool moveLeft = sliderPosition < _previousSliderPosition;
+
+    if (moveLeft) {
+      leftControlPoint1 -= bend;
+      leftControlPoint2 += bend;
+      rightControlPoint1 += bend;
+      rightControlPoint2 -= bend;
+      centerPoint += bend;
+    } else {
+      leftControlPoint1 += bend;
+      leftControlPoint2 -= bend;
+      rightControlPoint1 -= bend;
+      rightControlPoint2 += bend;
+      centerPoint -= bend;
+    }
+
+    var waveCurve = WaveCurveDefinitions(
+      startOfBezier: startOfBezier,
+      endOfBezier: endOfBezier,
+      leftControlPoint1: leftControlPoint1,
+      leftControlPoint2: leftControlPoint2,
+      rightControlPoint1: rightControlPoint1,
+      rightControlPoint2: rightControlPoint2,
+      controlHeight: controlHeight,
+      centerPoint: centerPoint,
+    );
+    return waveCurve;
+  }
+
+  _paintWaveLine(Canvas canvas, Size size) {
+    WaveCurveDefinitions waveCurve = _calculateWaveCurveDefinitions();
+
     Path path = Path()
       ..moveTo(0, size.height)
-      ..lineTo(
-        startOfBezier,
-        size.height,
-      )
+      ..lineTo(waveCurve.startOfBezier, size.height)
       ..cubicTo(
-        leftControlPoint1,
-        size.height,
-        leftControlPoint2,
-        controlHeight,
-        centerPoint,
-        controlHeight,
-      )
+          waveCurve.leftControlPoint1,
+          size.height,
+          waveCurve.leftControlPoint2,
+          waveCurve.controlHeight,
+          waveCurve.centerPoint,
+          waveCurve.controlHeight)
       ..cubicTo(
-        rightControlPoint1,
-        controlHeight,
-        rightControlPoint2,
-        size.height,
-        endOfBezier,
-        size.height,
-      )
-      ..lineTo(
-        size.width,
-        size.height,
-      );
+          waveCurve.rightControlPoint1,
+          waveCurve.controlHeight,
+          waveCurve.rightControlPoint2,
+          size.height,
+          waveCurve.endOfBezier,
+          size.height)
+      ..lineTo(size.width, size.height);
 
     canvas.drawPath(path, wavePainter);
   }
@@ -176,5 +222,30 @@ class WavePainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
+  bool shouldRepaint(WavePainter oldDelegate) {
+    _previousSliderPosition = oldDelegate.sliderPosition;
+    return true;
+  }
+}
+
+class WaveCurveDefinitions {
+  final double startOfBezier;
+  final double endOfBezier;
+  final double leftControlPoint1;
+  final double leftControlPoint2;
+  final double rightControlPoint1;
+  final double rightControlPoint2;
+  final double controlHeight;
+  final double centerPoint;
+
+  WaveCurveDefinitions({
+    this.startOfBezier,
+    this.endOfBezier,
+    this.leftControlPoint1,
+    this.leftControlPoint2,
+    this.rightControlPoint1,
+    this.rightControlPoint2,
+    this.controlHeight,
+    this.centerPoint,
+  });
 }
